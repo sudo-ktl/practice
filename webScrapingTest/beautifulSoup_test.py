958b3fe5620bab4918fc42f33208617788efd2ff
Kazuki Sudo <sudo@kyototextlab.com>
2023-06-26 19:51:14 +0900

    6.26 scraping

--- webScrapingTest/beautifulSoup_test.py
+++ webScrapingTest/beautifulSoup_test.py
@@ -1,8 +1,32 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
-url = 'https://www.yomiuri.co.jp/'
+ua = UserAgent()
+header = {'user-agent': ua.chrome}
 
-res = requests.get(url)
+# url = 'https://www.yomiuri.co.jp/'
 
-soup = BeautifulSoup(res.text, "html.parser")
+# res = requests.get(url,headers=header)
+
+# soup = BeautifulSoup(res.text, "html.parser")
+
+# elems = soup.select('body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(1) > div > h3 > a')
+
+# print(elems[0].contents[0])
+# print(elems[0].attrs['href'])
+
+url = 'https://www.yahoo.co.jp/'
+
+res = requests.get(url,headers=header)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+
+elems = soup.find_all(href=re.compile('news.yahoo.co.jp/pickup'))
+
+# print(elems[0].find('h1').find('span').contents[0])
+
+for elem in elems:
+    print(elem.find('h1').find('span').contents[0])
+    print(elem.attrs['href'])
