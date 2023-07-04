e646775aa3388d37a6a8741702750ca682385ddf
Kazuki Sudo <sudo@kyototextlab.com>
2023-07-04 16:33:40 +0900

    python

---
+++ python_sakai/sec3.py
@@ -0,0 +1,23 @@
+import math
+
+
+num = 1
+name = '1'
+
+print(num, type(num))
+print(name, type(name))
+
+num = name
+
+print(num, type(num))
+
+new_num = int(name)
+
+print(new_num, type(new_num))
+
+print('Hi', 'Mike', sep=',')
+
+# print(help(math))
+
+print('I don\'t know')
+print('say "I don\'t know"')
--- webScrapingTest/beautifulSoup_test.py
+++ webScrapingTest/beautifulSoup_test.py
@@ -6,17 +6,6 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 header = {'user-agent': ua.chrome}
 
-# url = 'https://www.yomiuri.co.jp/'
-
-# res = requests.get(url,headers=header)
-
-# soup = BeautifulSoup(res.text, "html.parser")
-
-# elems = soup.select('body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(1) > div > h3 > a')
-
-# print(elems[0].contents[0])
-# print(elems[0].attrs['href'])
-
 url = 'https://www.yahoo.co.jp/'
 
 res = requests.get(url,headers=header)
@@ -25,8 +14,23 @@ soup = BeautifulSoup(res.text, 'html.parser')
 
 elems = soup.find_all(href=re.compile('news.yahoo.co.jp/pickup'))
 
-# print(elems[0].find('h1').find('span').contents[0])
+print(elems)
 
 for elem in elems:
-    print(elem.find('h1').find('span').contents[0])
+    print(elem.find('span').contents[0])
     print(elem.attrs['href'])
+
+# pickup_links = [elem.attrs['href'] for elem in elems]
+# print(pickup_links)
+
+# for pickup_link in pickup_links:
+#     pickup_res = requests.get(pickup_link,headers=header)
+#     pickup_soup = BeautifulSoup(pickup_res.text, 'html.parser')
+#     pickup_elem  = pickup_soup.find('p', class_= 'sc-JnCVy hiAeLc')
+#     news_link = pickup_elem.contents[0].attrs['href']
+#     news_res = requests.get(news_link)
+#     news_soup = BeautifulSoup(news_res.text, 'html.parser')
+#     print(news_soup.find('title').contents[0])
+#     print(news_link)
+#     detail_text = news_soup.find(class_=re.compile('highLightSearchTarget'))
+#     print(detail_text.text if hasattr(detail_text,"text") else '', end='\n\n\n\n')
